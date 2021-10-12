1c0bc749dfd2b7212455f3e5a832187b05b061e2
Daniel Molina <dmolcap@gmail.com>
2021-10-12 20:36:38 +0200

    Add API call and index refactor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from "./components/search_bar";
 
 const API_KEY = 'YOUR_YOUTUBE_API_KEY'; // Youtube API key
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, search: 'surfboards' }, (videos) => {
+      this.setState({ videos }); // this is the same that this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
