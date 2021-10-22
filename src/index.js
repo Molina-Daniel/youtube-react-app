e97c5a519dd6c0a66ba7784726ad8013e4316bb5
Daniel <dmolcap@gmail.com>
2021-10-22 23:03:54 +0100

    Add video list components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = 'YOUR_YOUTUBE_API_KEY'; // Youtube API key
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
